62e5d9e2e2c1f2396f7a896ccceb7ebb108c9f48
m1000-isa <axelisaacjimenezramirez@gmail.com>
2026-02-10 19:37:25 -0800

    Descripcion del cambio que hiciste en el archivo README.md

--- isaac.py
+++ isaac.py
@@ -5,3 +5,23 @@ print(type(7))
 print(type(7.7))
 print(int(1+2))
 print(int(10*2))
+
+print(int(2**3))
+print(int(4**8))
+print(float(10%2))
+print(float(25%4))
+
+ventas = 1999999
+print("muestras vestas fueron ", ventas)
+
+is_active = True
+print(bool(is_active))
+
+game_over = False
+print(game_over)
+
+edad = 16
+if (edad >= 18):
+    print("Si puedo entrar a el Bar!")
+else:
+    print("No puedes entrar al bar!")

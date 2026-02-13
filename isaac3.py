5d8644b5b6f4b86cd486e231969889b942a5f98c
m1000-isa <axelisaacjimenezramirez@gmail.com>
2026-02-13 15:54:00 -0800

    Descripcion del cambio que hiciste en el archivo README.md

--- isaac3.py
+++ isaac3.py
@@ -17,3 +17,23 @@ mi_lista_2 = {"lunes","martes","miercoles","Jueves","viernes"}
 for i in mi_lista_2:
     if i !="lunes":
         print(f"Feliz {i}!")
+
+# while loop
+i = 0
+
+while i < 5:
+    i += 1
+    if i==3:
+        continue
+    print(i)
+    if i == 4:
+        break
+else:
+    print("i es ahora el mayor o igual a 5")
+
+count = 0
+while count < 3:
+    for i in mi_lista_2:
+        if i != "lunes":
+            print(i)
+    count += 1

5f732a4fa6dd9b7af2b1166979469b1625fb28de
m1000-isa <axelisaacjimenezramirez@gmail.com>
2026-02-12 18:38:16 -0800

    Descripcion del cambio que hiciste en el archivo README.md

---
+++ isaac2.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_tupla = (2,4)
+print("Mi tupla: ", mi_tupla)
+
+#Lista
+mi_lista = [1,3.1416, "Chistiian", mi_tupla]
+print("El primer elemento de mi lista: ", mi_lista[0])
+print("El cuarto elemento de mi lista: ", mi_lista[3])
+print("El tercer elemento de mi lista: ", mi_lista[2])

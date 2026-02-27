0a5866fa61c5eeffcfae78758b54833880e978cd
m1000-isa <axelisaacjimenezramirez@gmail.com>
2026-02-27 15:43:29 -0800

    terminado

--- OOP/Zombie.py
+++ OOP/Zombie.py
@@ -1,11 +1,12 @@
 from Enemigo import *
+import random 
 
 class Zombie:
-    def __init__(self, tipo, puntos_energia, ataque):
-        self.tipo = tipo
+    def __init__(self, puntos_energia, nivel, ataque=3):
+        self.tipo = "Zombie"
         self.puntos_energia = puntos_energia
+        self.nivel = nivel
         self.ataque = ataque
-
     def get_tipo_enemigo(self):
         return self.tipo
     def habla(self):
@@ -13,3 +14,9 @@ class Zombie:
 
     def propagar_enfermedades(self):
             print("El Zombie esta tratando de propagar la enfermedad!!")
+    def ataque_especial(self):
+          print("Zombie ataque especial")
+          funciona_ataque_especial = random.random() <0.50
+          if funciona_ataque_especial:
+                self.ataque += 2
+                print('Zombie ha generado su energia con 2HP!!!')
